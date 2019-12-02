0fef8b5d284e1d9831b7691783c042e758714f28
SC <a@b.com>
2019-12-02 15:33:28 -0600

    temp comment

--- back-end/src/GoogleCloudData.ts
+++ back-end/src/GoogleCloudData.ts
@@ -1,43 +1,8 @@
 import { resolve } from "dns";
 
-const speech = require('@google-cloud/speech');
-const language = require('@google-cloud/language');
 const fs = require('fs');
 const path = require('path')
 const Ffmpeg = require('fluent-ffmpeg')
-
-var speechClient = new speech.SpeechClient();
-var langClient = new language.LanguageServiceClient();
-
-
-//TODO: Clean up if possible
-async function sendToGoogleCloud(fileName) : Promise<string>{
-  return new Promise(async (resolve,reject) =>{
-    // Reads a local audio file and converts it to base64
-    const file = fs.readFileSync(fileName);
-    const audioBytes = file.toString('base64');
-    
-    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-    const audio = {
-      content: audioBytes,
-    };
-    const config = {
-      languageCode: 'en-US',
-      audio_channel_count: 1,
-      enableAutomaticPunctuation: true,
-    };
-    const request = {
-      audio: audio,
-      config: config,
-    };
-    const [response] = await speechClient.recognize(request);
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log(transcription)
-    resolve(transcription)
-  })
-}
 
 class GoogleCloudData{
   private _transcript: string = "N/A"
@@ -61,10 +26,7 @@ class GoogleCloudData{
         .audioChannels(1)
         .output(newFileName)
         .on('end', function(){
-          sendToGoogleCloud(newFileName).then(transcript =>{
-            //this.Transcript = transcript //TODO: This setter actually doesn't work.
-            resolve(transcript);
-          });
+          resolve("This is the greatest speech ever. It should always work.")
         })
         .on('error', e => {
           reject(e)
@@ -79,7 +41,6 @@ class GoogleCloudData{
   set Transcript(new_transcript: string){
     this._transcript = new_transcript;
   }
-
 }
 module.exports=GoogleCloudData;
 
