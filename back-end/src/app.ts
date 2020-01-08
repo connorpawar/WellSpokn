fca50092ea23601dc5a88b6ab8fe5227c0477e1e
SC <a@b.com>
2020-01-08 16:04:20 -0600

    Enviroment Separation complete

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,15 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs')
 const cors = require('cors')
-const path = require('path')
-const https = require('https')
 
-
-import Router from 'routes/routes';
+import Router from './routes/routes';
 
 const app = express();
-const port = 8080;
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,8 +12,13 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-//TODO: Is this okay?
-app.use("/api",Router) 
-app.use("/",Router)
+const env : string  = process.env.NODE_ENV || 'development'
 
-app.listen(8080)
+if (env == 'development'){
+    app.use("/api",Router)
+    app.listen(5000)
+}
+if (env == 'production'){
+    app.use("/",Router)
+    app.listen(8080)
+}
