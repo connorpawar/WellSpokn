750468c91b0f7882f68ac1799e0d60235b93a0a7
SC <a@b.com>
2020-01-21 11:46:06 -0600

    Production Enviroment and Development Enviroment separated further

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
+const config = require('config');
+
 
 import Router from './routes/routes';
 
@@ -12,13 +14,5 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-const env : string  = process.env.NODE_ENV || 'development'
-
-if (env == 'development'){
-    app.use("/api",Router)
-    app.listen(5000)
-}
-if (env == 'production'){
-    app.use("/",Router)
-    app.listen(8080)
-}
+app.use(config.get("ROOT_ROUTE"),Router)
+app.listen(config.get("PORT"))
