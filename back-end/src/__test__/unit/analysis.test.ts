80c427aa970966211dc78aa67c337be9c678ee2d
SC <a@b.com>
2020-01-29 14:01:27 -0600

    Analysis core is now asynchous

--- back-end/src/__test__/unit/analysis.test.ts
+++ back-end/src/__test__/unit/analysis.test.ts
@@ -1,12 +1,20 @@
 import { AnalysisComponent } from "../../analysis/AnalysisComponent";
 import AnalysisCore from '../../analysis/AnalysisCore';
+import { StringifyOptions } from "querystring";
+
+
+function resolveWrap<T>(data : T) : Promise<T> {
+    return new Promise((resolve,reject) =>{
+        resolve(data);
+    })
+}
 
 class StubAB extends AnalysisComponent<string,string>{
     inputTopic = "A";
     outputTopic = "B";
 
-    analyze(data: string) : string {
-        return data + "\nAB-Proccesed";
+    analyze(data: string) : Promise<string> {
+        return resolveWrap<string>(data + "\nAB-Proccesed");
     };
 }
 
@@ -14,8 +22,8 @@ class StubBC extends AnalysisComponent<string,string>{
     inputTopic = "B";
     outputTopic = "C";
 
-    analyze(data: string) : string {
-        return data + "\nBC-Proccesed";
+    analyze(data: string) : Promise<string> {
+        return resolveWrap<string>(data + "\nBC-Proccesed");
     };
 }
 
@@ -23,8 +31,8 @@ class StubASplit extends AnalysisComponent<string,string>{
     inputTopic = "B";
     outputTopic = "ASplit";
 
-    analyze(data: string) : string {
-        return "ASplit";
+    analyze(data: string) : Promise<string> {
+        return resolveWrap<string>("ASplit");
     };
 }
 
@@ -32,8 +40,17 @@ class StubBSplit extends AnalysisComponent<string,string>{
     inputTopic = "B";
     outputTopic = "BSplit";
 
-    analyze(data: string) : string {
-        return "BSplit";
+    analyze(data: string) : Promise<string> {
+        return resolveWrap<string>("BSplit");
+    };
+}
+
+class Stub extends AnalysisComponent<string,string>{
+    inputTopic = "";
+    outputTopic = "";
+
+    analyze(data: string) : Promise<string> {
+        return resolveWrap<string>(this.inputTopic + this.outputTopic);
     };
 }
 describe('AnalysisCore class', () => {
@@ -44,14 +61,14 @@ describe('AnalysisCore class', () => {
     var initialInput = {"A": "Data"};
     core.addAnalysisComponent<string,string>(abPart)
     core.addAnalysisComponent<string,string>(bcPart)
-    core.intialize("A",initialInput);
+    await core.intialize("A",initialInput);
     
     expect(initialInput["A"]).toEqual("Data")
     expect(initialInput["B"]).toEqual("Data\nAB-Proccesed")
     expect(initialInput["C"]).toEqual("Data\nAB-Proccesed\nBC-Proccesed")
     done()
   });
-  test('initialize path with splites', async (done) => {
+  test('initialize path with splits', async (done) => {
     var core = new AnalysisCore()
     var abPart = new StubAB();
     var bcPart = new StubBC();
@@ -62,13 +79,32 @@ describe('AnalysisCore class', () => {
     core.addAnalysisComponent<string,string>(bcPart)
     core.addAnalysisComponent<string,string>(aSplitPart)
     core.addAnalysisComponent<string,string>(bSplitPart)
-    core.intialize("A",initialInput);
+    await core.intialize("A",initialInput);
     
     expect(initialInput["A"]).toEqual("Data")
     expect(initialInput["B"]).toEqual("Data\nAB-Proccesed")
     expect(initialInput["C"]).toEqual("Data\nAB-Proccesed\nBC-Proccesed")
     expect(initialInput["ASplit"]).toEqual("ASplit")
     expect(initialInput["BSplit"]).toEqual("BSplit")
+    done()
+  });
+  test('initialize long path', async (done) => {
+    var core = new AnalysisCore()
+    var stringy = "QAZXSWEDCVFRTGBNHYUJMKIOPL"
+    for (var i = 1; i < stringy.length; i++) {
+        var component = new Stub();
+        component.inputTopic = stringy.substring(i-1,i)
+        component.outputTopic = stringy.substring(i,i+1)
+        core.addAnalysisComponent<string,string>(component)
+    }
+
+    var initialInput = {"Q": "QA"};
+    await core.intialize("Q",initialInput);
+    
+    for (var i = 2; i < stringy.length; i++) {
+        expect(initialInput[stringy.substring(i-1,i)]).toEqual(stringy.substring(i-2,i))
+    }
+
     done()
   });
 });
