6c4b48ec469bcb407824e2408b2a4e407829287c
SC <a@b.com>
2020-01-29 14:44:43 -0600

    Breaking up google cloud data

---
+++ back-end/src/analysis/FileToAudioBytesComponent.ts
@@ -0,0 +1,43 @@
+import { AnalysisComponent } from "./AnalysisComponent"
+
+const fs = require('fs');
+const path = require('path')
+const Ffmpeg = require('fluent-ffmpeg')
+
+class FileToAudioBytesComponent extends AnalysisComponent<string,string>{
+  inputTopic = "audioFile";
+  outputTopic = "audioBytes";
+
+  replaceExtension(fileName) : string{
+    var fileNameExt : string = path.parse(fileName).ext;
+    fileName = fileName.replace(fileNameExt,"");
+    fileName += ".wav";
+    return fileName;
+  }s
+
+  analyze(fileName : string) : Promise<string>{
+    return new Promise((resolve,reject) => {
+      var newFileName : String = this.replaceExtension(fileName);
+      var command = new Ffmpeg()
+        .input(fileName)
+        .audioChannels(1)
+        .output(newFileName)
+        .on('end', async function(){
+          const file = fs.readFileSync(newFileName);
+          const audioBytes = file.toString('base64');
+          fs.unlink(fileName,() => {})
+          fs.unlink(newFileName,() => {})
+          resolve(audioBytes);
+        })
+        .on('error', e => {
+          fs.unlink(fileName,() => {})
+          reject(e)
+        });
+      command.run()
+    });
+  }
+
+
+
+}
+export default FileToAudioBytesComponent;
