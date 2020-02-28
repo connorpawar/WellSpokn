b24dc2cdf1ed1ec764682b466d1c023287f77267
SC <a@b.com>
2020-02-27 19:03:41 -0600

    Rename

--- back-end/src/__test__/integration/SendToGoogleCloud.integration.test.ts
+++ back-end/src/__test__/integration/SendToGoogleCloud.integration.test.ts
@@ -1,11 +1,13 @@
 import AnalysisCore from '../../analysis/AnalysisCore';
-import GoogleTextToSpeechComponent from '../../analysis/GoogleSpeechToTextComponent';
+import FileToAudioBytesComponent from '../../analysis/FileToAudioBytesComponent';
+import GoogleSpeechToTextComponent from '../../analysis/GoogleSpeechToTextComponent';
 import GoogleNaturalLanguageComponent, {Sentiment} from '../../analysis/GoogleNaturalLanguageComponent';
 
 
 describe('Can send file to Google Cloud', () => {  
   test('GoogleCloudData replaceExtension works', async (done) => {
     var analysisCore = new AnalysisCore();
+    analysisCore.addAnalysisComponent<string>(new FileToAudioBytesComponent())
     analysisCore.addAnalysisComponent<string>(new GoogleSpeechToTextComponent())
     analysisCore.addAnalysisComponent<Sentiment>(new GoogleNaturalLanguageComponent())
     var initialData = {"audioFile" : "./files/loveChocolate"};
