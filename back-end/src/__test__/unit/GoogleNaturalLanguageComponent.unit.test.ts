a7438924cce6c569c04ae59adc5b4708a32e682b
Your Name <b>
2020-02-28 18:24:20 -0600

    Fixed

--- back-end/src/__test__/unit/GoogleNaturalLanguageComponent.unit.test.ts
+++ back-end/src/__test__/unit/GoogleNaturalLanguageComponent.unit.test.ts
@@ -10,7 +10,7 @@ function resolveWrap(arg){
 
 describe('GoogleNaturalLanguageComponent class', () => {
   test('GoogleNaturalLanguageComponent analyze works', async (done) => {
-    var expectedReturn = sentimentData.documentSentiment;
+    var expectedReturn = sentimentData;
     const inputData = {
       transcript : "I hate coconut flavored foods. I prefer chocolate flavored things. In fact, I love pizza, especially cheese and sausage pizza."
     }
@@ -31,7 +31,7 @@ describe('GoogleNaturalLanguageComponent class', () => {
     );
 
     var componentReturn = await component.analyze(inputData)
-    expect(componentReturn).toEqual(expectedReturn.documentSentiment)
+    expect(componentReturn[0].documentSentiment).toEqual(expectedReturn[0].documentSentiment)
     
     done();
   });
