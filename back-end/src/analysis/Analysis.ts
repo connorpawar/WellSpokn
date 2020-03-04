1f8b28b670c6571c091c23b2c8aead9b05699067
kylelindteigen <kylelindteigen@ku.edu>
2020-03-03 19:19:55 -0600

    Languagetool should work now need to test

--- back-end/src/analysis/Analysis.ts
+++ back-end/src/analysis/Analysis.ts
@@ -1,12 +1,14 @@
 import AnalysisCore from "./AnalysisCore";
 import FileToAudioBytesComponent from './FileToAudioBytesComponent'
 import GoogleTextToSpeechComponent from './GoogleTextToSpeechComponent'
+import LanguageToolComponent from './LanguageToolComponent'
 
 //TODO: Is this the best place to initialize everything?
 function generateAnalysisCore() : AnalysisCore{
     var analysisCore = new AnalysisCore();
     analysisCore.addAnalysisComponent<string>(new FileToAudioBytesComponent())
     analysisCore.addAnalysisComponent<string>(new GoogleTextToSpeechComponent())
+    analysisCore.addAnalysisComponent<Object>(new LanguageToolComponent())
     return analysisCore
 }
 
