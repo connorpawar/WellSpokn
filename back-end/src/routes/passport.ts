dc75d9995ff7a023886c698ea055c8e4284fa544
Your Name <you@example.com>
2020-03-30 10:06:45 -0500

    Implementing passport

--- back-end/src/routes/passport.ts
+++ back-end/src/routes/passport.ts
@@ -2,37 +2,44 @@ import sql from '../database/sql';
 
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
 
 passport.use(
-    new Strategy(
-        async function(username, password, cb) {
-            try{
-                var user = await sql.getUser(username)
-    
-                if(!user){
+    new Strategy({
+            usernameField : 'username',
+            passwordField : 'password'
+        },
+        function(username, password, cb) {
+            sql.getUser(username).then(u =>{
+                if(!u){
                     return cb(null,false)
                 }
+                bcrypt.compare(password,u.password, function(err,match){
+                    if(err) throw err;
+                    if(match){
+                        return cb(null,u)
+                    }else{
+                        return cb(null,false)
+                    }
+                })
+            }).catch(e => {
+                return cb(null,false)
+            })
     
-                if(user.password == password){
-                    return cb(null,user)
-                }
-            }catch(e){
-                return cb(e)
-            }
         }
     )
 );
-  
+
+//TODO: use ID instead of username
 passport.serializeUser(function(user, cb) {
     cb(null, user.username);
 });
-passport.deserializeUser(
-    async function(username, cb) {
-        try{
-            var user = await sql.getUser(username)
-            return cb(null,user)
-        }catch(e){
-            return cb(e)
-        }
-    }
+passport.deserializeUser(function(username, cb) {
+    sql.getUser(username).then(u =>{
+        cb(null,u)
+    }).catch(e =>{
+        cb(e,false)
+    });
 });
+
+export default passport;
