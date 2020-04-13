612da1b0cca4a4dedee5f25dfdcd846a1aa8ac66
Your Name <you@example.com>
2020-04-13 12:54:31 -0500

    using push instead of concat; also reworded errors

--- back-end/src/routes/speech.ts
+++ back-end/src/routes/speech.ts
@@ -26,61 +26,6 @@ Router.get('/speech/:id',  function (req, res) {
     getSpecificSpeech(res,userId,speechId)
 });
 
-
-//TODO: Uncomment later
-/*
-Router.post('/newAttempt/:id?', upload.single('audio'), async (req,res) =>{
-    var id = req.params.id
-    sql.finalizeAttempt(id).then(() =>{
-        res.send("h")
-    })
-});
-
-function dummyDataPopulation(req,res,speech){
-    var sid = speech.id
-    var uid = speech.user_id
-    var initialData = {"transcript" : speech.transcript};
-    var promises = []
-    var analysisCore = generateAnalysisCore()
-    analysisCore.intialize("transcript",initialData).then(async (allData : any) => { 
-        console.log(allData)  
-        for(var x of allData.languageToolErrors.matches){
-            var err = sql.addError(sid, x.rule.category.name, x.context.offset, x.context.offset + x.context.length, x.rule.description);
-            promises.concat(err)
-        }
-        Promise.all(promises).then(()=>{
-            console.log(speech)
-            sql.finalizeAttempt(sid).then(() =>{
-                getSpecificSpeech(res,uid,sid)
-            })
-        });
-    }).catch( e =>{
-        console.log(e)
-    })   
-}
-
-Router.post('/dev_speech/:sid?',  (req, res) => {
-    const json_data = req.body
-    var email = req.user.email
-    var title = json_data.title
-    var transcript = json_data.transcript
-    var sid = req.params.sid
-    var promise
-    if(sid){
-        promise = sql.upsertSpeech(email,title,transcript,sid)
-    }else{
-        promise = sql.upsertSpeech(email,title,transcript)
-    }
-
-    promise.then(s =>{
-        dummyDataPopulation(req,res,s)
-    }).catch(e => {
-        console.log(e)
-        res.send("Speech could not be created.");
-    })
-});
-*/
-
 Router.get('/speech_previews',  function (req, res) {
     var email = req.user.email
     sql.getAllSpeechesForASpecificUser(email).then(data => {
@@ -113,27 +58,27 @@ Router.post('/speech/:sid?', upload.single('audio'), async (req,res) =>{
             
             //TODO Modifiy thresholds as needed.
             if(allData.wordsPerMinute > 150){
-                promises.concat(sql.addError(id, "Speed", 0, 0, "Your words per minute is " + allData.wordsPerMinute + ". Your words per minute should be at most 160 for presentations. If you are around this speed, consider slowing down."));
+                promises.push(sql.addError(id, "Speed", 0, 0, "Your words per minute is " + allData.wordsPerMinute + ". Your words per minute should be at most 160 for presentations. If you are around this speed, consider slowing down."));
             }else if(allData.wordsPerMinute < 100){
-                promises.concat(sql.addError(id, "Speed", 0, 0, "Your words per minute is " + allData.wordsPerMinute + ". Your words per minute should be at least 100 for presentations. If you are around this speed, consider speeding up."));
+                promises.push(sql.addError(id, "Speed", 0, 0, "Your words per minute is " + allData.wordsPerMinute + ". Your words per minute should be at least 100 for presentations. If you are around this speed, consider speeding up."));
             }
             
             //TODO Modifiy thresholds as needed.
             var sentimentScore = allData.sentiment[0].documentSentiment.score;
             if(allData.sentiment[0].documentSentiment.score > 0.5){
-                promises.concat(sql.addError(id, "Sentiment", 0, 0, "Your sentiment score is " + sentimentScore + ". This means you had mostly positive things to say; this being emotionally positive a goal of your speech?"));
+                promises.push(sql.addError(id, "Sentiment", 0, 0, "Your sentiment score is " + sentimentScore + ". This means you had mostly positive things to say; is being emotionally positive a goal of your speech?"));
             }else if(allData.sentiment[0].documentSentiment.score < -0.5){
-                promises.concat(sql.addError(id, "Sentiment", 0, 0, "Your sentiment score is " + sentimentScore + ". This means you had mostly negative things to say; this being emotionally negative a goal of your speech?"));
+                promises.push(sql.addError(id, "Sentiment", 0, 0, "Your sentiment score is " + sentimentScore + ". This means you had mostly negative things to say; is being emotionally negative a goal of your speech?"));
             }
 
             for(var x of allData.languageToolErrors.matches){
                 var errPromise = sql.addError(id, x.rule.category.name, x.context.offset, x.context.offset + x.context.length, x.rule.description);
-                promises.concat(errPromise);
+                promises.push(errPromise);
             }
             console.log(errPromise)
             Promise.all(promises).then(() =>{
                 sql.finalizeAttempt(id).then(() =>{
-                    res.send(s)
+                    getSpecificSpeech(res,s.user_id,s.id)
                 })
             })
         })
