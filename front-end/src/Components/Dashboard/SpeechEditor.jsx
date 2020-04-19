32f7816da1bf5c689c00c3cfeb99f2ab2e868d27
connorpawar <pawar@ku.edu>
2020-04-19 11:51:21 -0500

    added global errors card and added editing speeches

--- front-end/src/Components/Dashboard/SpeechEditor.jsx
+++ front-end/src/Components/Dashboard/SpeechEditor.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import { makeStyles } from '@material-ui/core/styles';
 import { Editor, EditorState, ContentState, CompositeDecorator } from 'draft-js';
 
+import LoadingScreen from '../Layout/LoadingScreen';
+
+const useStyles = makeStyles((theme) => ({
+	button: {
+	  margin: theme.spacing(1),
+	},
+  }));
 
 export default function SpeechEditor(props) {
+	const classes = useStyles();
 
 	let counts = props.counts;
 
 	const [errors, setErrors] = React.useState(props.errors);
+	const [load, setLoad] = React.useState(false);
+
+	const handleCorrection = (editorState) =>{
+		let content = editorState.getCurrentContent();
+		let text = content.getPlainText();
+
+		var form_data = new FormData();
+			form_data.append('transcript', text);
+			fetch('../api/speech_fix/' + props.id, {
+				method: 'POST',
+				body: form_data
+			}).then(response => response.json())
+			.then(JSONresponse => {props.setChangedSpeech(true); setLoad(false);})
+			.catch(error => console.log("fetch error", error));
+	}
 
 	const HighlightedErrors = (props) => {
 
@@ -61,9 +87,20 @@ export default function SpeechEditor(props) {
 	);
 
 	return (
-		<Editor
-			editorState={editorState}
-			onChange={setEditorState}
-		/>
+		<React.Fragment>
+			<Editor
+				editorState={editorState}
+				onChange={setEditorState}
+				stripPastedStyles={true}
+			/>
+			<Button
+			variant="contained"
+			color="default"
+			className={classes.button}
+			startIcon={<SaveIcon />}
+			onClick={() =>{handleCorrection(editorState)}}
+			>Save Changes</Button>
+			<LoadingScreen loading={load}/>
+	  </React.Fragment>
 	);
 }
