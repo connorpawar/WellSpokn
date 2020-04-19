66705d8e839e3d99e7b067b280a50f7a70dcec5b
Your Name <you@example.com>
2020-04-19 18:50:20 -0500

    analysis

--- back-end/src/analysis/Analysis.ts
+++ back-end/src/analysis/Analysis.ts
@@ -1,5 +1,5 @@
 import AnalysisCore from "./AnalysisCore";
-import FileToAudioBytesComponent from './FileToAudioBytesComponent'
+import FfmpegFileConverterComponent from './FfmpegFileConverterComponent'
 import GoogleSpeechToTextComponent from './GoogleSpeechToTextComponent'
 import GoogleNaturalLanguageComponent, {Sentiment} from './GoogleNaturalLanguageComponent'
 import GoogleSyntaxComponent from './GoogleSyntaxComponent'
@@ -10,7 +10,7 @@ import HzComponent from './HzComponent'
 //TODO: Is this the best place to initialize everything?
 function generateAnalysisCore() : AnalysisCore{
     var analysisCore = new AnalysisCore();
-    analysisCore.addAnalysisComponent<string>(new FileToAudioBytesComponent())
+    analysisCore.addAnalysisComponent<string>(new FfmpegFileConverterComponent())
     analysisCore.addAnalysisComponent<string>(new GoogleSpeechToTextComponent())
     analysisCore.addAnalysisComponent<Sentiment>(new GoogleNaturalLanguageComponent())
     analysisCore.addAnalysisComponent<number>(new WordsPerMinuteComponent())
