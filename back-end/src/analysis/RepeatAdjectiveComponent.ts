2b77f2de9691df7773c394b0a4484a0faa966be4
Your Name <you@example.com>
2020-04-19 22:08:59 -0500

    Undebug and debug

--- back-end/src/analysis/RepeatAdjectiveComponent.ts
+++ back-end/src/analysis/RepeatAdjectiveComponent.ts
@@ -1,14 +1,13 @@
 const language = require('@google-cloud/language');
 import { AnalysisComponent } from "./AnalysisComponent"
 
-const language : any = require('@google-cloud/language');
 
-class RepeatAdjectiveComponent extends AnalysisComponent<Sentiment>{
+class RepeatAdjectiveComponent extends AnalysisComponent<any>{
   languageClient = new language.LanguageServiceClient();
   inputTopic = new Set(["syntaxanalysis"]);
   outputTopic = "repeatAdjectiveErrors";
 
-  analyze(data: Array) : Promise<Sentiment>{
+  analyze(data: Array<any>) : Promise<any>{
     var syntax = data["syntaxanalysis"];
     return new Promise((resolve,reject) => {
 
